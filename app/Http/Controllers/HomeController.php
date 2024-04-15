0e4f3d8c09a0764497656a8ed67c3af1ae164523
Alberto-1710 <ajromero1710@gmail.com>
2024-04-14 21:52:56 -0600

    Prueba guzzle

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,13 +1,40 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+
+
 
 class HomeController extends Controller
 {
     public function home($nombre){
         return view("home", compact("nombre"));
     }
-}
 
+    public function prueba()
+    {
+        // Crear una instancia de Guzzle Client
+        $client = new Client([
+            'base_uri' => 'http://localhost:8787/api', // Reemplaza con la URL de tu proyecto Spring Boot
+            'timeout'  => 2.0, // Tiempo máximo de espera en segundos
+        ]);
+
+        try {
+            // Realizar una solicitud GET a una ruta específica en tu aplicación Spring Boot
+            $response = $client->request('GET', 'prueba');
+            
+            // Obtener el código de estado de la respuesta
+            $statusCode = $response->getStatusCode();
+            
+            // Obtener el cuerpo de la respuesta como una cadena JSON
+            $body = $response->getBody()->getContents();
+            
+            // Puedes manejar la respuesta aquí según tus necesidades
+            return $body;
+        } catch (\Exception $e) {
+            // Manejar cualquier error que ocurra durante la solicitud
+            return $e->getMessage();
+        }
+    }
+}
